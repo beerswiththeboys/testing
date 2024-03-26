7dfb82e4898bd2391e39d627cb4ebcff4a6a9590
beerswiththeboys <162860896+beerswiththeboys@users.noreply.github.com>
2024-03-26 10:23:34 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 
-export const leagueID = "your_league_id"; // your league ID
+export const leagueID = "1048182381779218432"; // your league ID
 export const leagueName = "Beers With The Boys"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper

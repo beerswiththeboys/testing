dedad749717139c48728712c7401f1deb3e825b7
beerswiththeboys <162860896+beerswiththeboys@users.noreply.github.com>
2024-03-27 00:53:57 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 
-export const leagueID = ""; // your league ID
-export const leagueName = ""; // your league name
+export const leagueID = "1048182381779218432"; // your league ID
+export const leagueName = "Beers With The Boys"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
